227340dbb8b7b8f517c93b440d556ac77941d886
TMiya3 <is0283ir@ed.ritsumei.ac.jp>
2019-08-20 19:06:55 +0900

    FのT.Mの回答をとりあえず追加

---
+++ F_Find_a_Friend/T.M/main.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int rp[100010][110],rn[100010][110],rt[100010][110];
+int main(){
+  int n;
+  scanf("%d %d %d %d",&n,&m,&mt,&p);
+  for(i=0;i<n;i++){
+    scanf("%d %d %d",&q[i],&a,&t[i]);
+    if(q[i]){
+      for(j=0;r[i][j]-a;j++);
+      r[i][j]=-1;
+      nt[b]=i;
+      b=i;
+    }
+    else{
+      for(j=0;r[i][j]>-1;j++);
+      rp[i][j]=a;
+      rt[i][j]=t[i];
+    }
+  }
+  for(i=0;i<n;i++){
+    a=fin[i];
+    while(a<m){
+      if(q[i]
+    
+    
+	
+      
+    
+  return 0;
+}
