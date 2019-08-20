f75516da65e9e9d20c83e25bdd06431bccde5e01
rika0384 <sora6kuro0f9@yahoo.co.jp>
2019-08-20 19:03:14 +0900

    fix F generator and constraints

--- F_Find_a_Friend/tests/constraints.hpp
+++ F_Find_a_Friend/tests/constraints.hpp
@@ -1,4 +1,12 @@
+#define MIN_N 2
+#define MAX_N 100000
+#define MIN_M 2
+#define MAX_M 100000
+#define MIN_T 1
+#define MAX_T 1000000000
 #define MIN_A 1
-#define MAX_A 1000
+#define MAX_A MAX_T
 #define MIN_B 1
-#define MAX_B 1000
+#define MAX_B MAX_N
+#define MIN_C 0
+#define MAX_C 1
--- F_Find_a_Friend/tests/generator.cc
+++ F_Find_a_Friend/tests/generator.cc
@@ -11,11 +11,25 @@ using namespace std;
 
 // aとbをファイルストリームに出力する
 // ファイル名は prefix_num.in (ex: 00_sample_00.in)
-void output(int a, int b, const string &prefix, int num){
+int a[MAX_M],b[MAX_M],c[MAX_M];
+void output(int n, int m, int t, const string &prefix, int num){
+    int flag[MAX_N+1] = {};
+    int num = 0;
+    rep(i,m){
+        a[i] = rnd.next(MIN_A, t);
+        b[i] = rnd.next(MIN_B,n);
+        while(flag[b[i]] == 0 && num == 100)b[i] = rnd.next(MIN_B,n);
+        c[i] = flag[b[i]] == 0;
+        if(c[i])num++;
+        else num--;
+        flag[b[i]] = 1 - flag[b[i]];
+    }
+    sort(a,a+m);
     char name[100];
     sprintf(name, "%s_%02d.in", prefix.c_str(), num);
     ofstream ofs(name);
-    ofs << a << " " << b << endl;
+    ofs << n << " " << m << " " << t << endl;
+    rep(i,m) ofs << a[i] << " " << b[i] << " " << c[i] << endl;
     ofs.close();
 }
 
@@ -24,22 +38,29 @@ int main(){
     // pidを足すことで、1秒以上間を置かずに起動したときに同じシードになってしまうのを防ぐ
     rnd.setSeed(time(0)+getpid());
 
-    // 乱数ケースを10個生成
-    for(int i = 0; i < 10; ++i){
-        int A = rnd.next(MIN_A, MAX_A);
-        int B = rnd.next(MIN_B, MAX_B);
-        output(A, B, "50_random", i);
-    }
 
-    // 片方が大きいケースを生成
     for(int i = 0; i < 10; ++i){
-        int A = 1;
-        int B = 1;
-        while(0.5*A <= B && B <= 1.5*A){
-            A = rnd.next(MIN_A, MAX_A);
-            B = rnd.next(MIN_B, MAX_B);
-        }
-        if(rnd.next(0,1)) swap(A, B);
-        output(A, B, "60_unbalance", i);
+        int n = rnd.next(MIN_N, 10);
+        int m = rnd.next(MIN_M, 10);
+        int t = rnd.next(MIN_T, 100);
+        output(n, m, t, "50_small", i);
+    }
+    for(int i = 0; i < 10; ++i){
+        int n = rnd.next(MIN_N, MAX_N);
+        int m = rnd.next(MIN_M, MAX_M);
+        int t = rnd.next(MIN_T, MAX_T);
+        output(n, m, t, "51_large", i);
+    }
+    for(int i = 0; i < 3; ++i){
+        int n = rnd.next(MIN_N, MIN_N);
+        int m = rnd.next(MIN_M, MIN_M);
+        int t = rnd.next(MIN_T, MIN_T);
+        output(n, m, t, "52_min", i);
+    }
+    for(int i = 0; i < 7; ++i){
+        int n = rnd.next(MAX_N, MAX_N);
+        int m = rnd.next(MAX_M, MAX_M);
+        int t = rnd.next(MAX_T, MAX_T);
+        output(n, m, t, "53_max", i);
     }
 }
