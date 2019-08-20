a17f2f2b3fd1d3605618e04b796c5863e5c62e15
rika0384 <sora6kuro0f9@yahoo.co.jp>
2019-08-20 18:20:18 +0900

    fix E constraints

--- E_Monster_Buster/tests/constraints.hpp
+++ E_Monster_Buster/tests/constraints.hpp
@@ -1,10 +1,10 @@
 #define MIN_N 1
 #define MAX_N 2000
-#define MIN_R 0
+#define MIN_R 1
 #define MAX_R 2000
 #define MIN_A -2000
 #define MAX_A 2000
 #define MIN_W -2000
 #define MAX_W 2000
-#define MIN_T 0
+#define MIN_T 1
 #define MAX_T 2000
