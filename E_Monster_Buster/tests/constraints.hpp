c0c74eef4304497db9d6a40066be399286fd986e
rika0384 <sora6kuro0f9@yahoo.co.jp>
2019-08-20 18:09:35 +0900

    fix E constraints and PROBLEM

--- E_Monster_Buster/tests/constraints.hpp
+++ E_Monster_Buster/tests/constraints.hpp
@@ -1,11 +1,10 @@
 #define MIN_N 1
-#define MAX_N 8
-#define MIN_R 1
-#define MAX_R 20
-#define MIN_A 1
-#define MAX_A 20
-#define MIN_W 1
-#define MAX_W 20
-#define MIN_T 1
-#define MAX_T 20
-
+#define MAX_N 2000
+#define MIN_R 0
+#define MAX_R 2000
+#define MIN_A -2000
+#define MAX_A 2000
+#define MIN_W -2000
+#define MAX_W 2000
+#define MIN_T 0
+#define MAX_T 2000
