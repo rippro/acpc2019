df9f06673c2426bfaf249a128d15cbea3401cfab
renkarsu <mutou0604@outlook.jp>
2019-08-23 17:05:45 +0900

    make validator

--- A/tests/validator.cc
+++ A/tests/validator.cc
@@ -1,12 +1,26 @@
 #include "./testlib.h"
 #include "./constraints.hpp"
 #include <cassert>
+#include<assert.h>
 
 int main(){
     registerValidation();
-    inf.readInt(MIN_A, MAX_A);
+    inf.readInt(MIN_X, MAX_X);
     inf.readSpace();
-    inf.readInt(MIN_B, MAX_B);
+    inf.readInt(MIN_T, MAX_T);
+    inf.readSpace();
+    inf.readInt(MIN_S, MAX_S);
     inf.readEoln();
+    int n = inf.readInt(MIN_N, MAX_N);
+    inf.readEoln();
+    bool flag = true;
+    for(int i = 0; i < n; ++i){
+        std::string s = inf.readString();
+        if(s != "nobiro" && s != "tidime" && s != "karero"){
+            flag = false;
+            assert(flag);
+        }
+        inf.readEoln();
+    }
     inf.readEof();
 }
