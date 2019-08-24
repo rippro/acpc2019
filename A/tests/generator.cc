62c73ce2775010290ca21dceba2c389333ac8112
xryuseix <ryusei143.shootingstar@gmail.com>
2019-08-24 20:21:01 +0900

    add C's validator

--- A/tests/generator.cc
+++ A/tests/generator.cc
@@ -9,18 +9,6 @@ using namespace std;
 
 #define rep(i,n) for(int i=0;i<(int)(n);i++)
 
-#define MIN_X 0
-#define MAX_X 100
-
-#define MIN_S -100
-#define MAX_S 100
-
-#define MIN_T -100
-#define MAX_T 100
-
-#define MIN_N 1
-#define MAX_N 1000
-
 #define MIN_Si 0
 #define MAX_Si 2
 
--- C/tests/validator.cc
+++ C/tests/validator.cc
@@ -2,11 +2,33 @@
 #include "./constraints.hpp"
 #include <cassert>
 
+#define MIN_A 2
+#define MAX_A 300
+
+#define MIN_B 2
+#define MAX_B 300
+
 int main(){
     registerValidation();
-    inf.readInt(MIN_A, MAX_A);
+    int n = inf.readInt(MIN_N, MAX_N);
     inf.readSpace();
-    inf.readInt(MIN_B, MAX_B);
+
+    int m = inf.readInt(MIN_M, n*(n - 1));
     inf.readEoln();
+    
+    bool flag = true;
+    
+    for(int i = 0; i < m; ++i) {
+        int a = inf.readInt(MIN_A, MAX_A);
+        inf.readSpace();
+        int b = inf.readInt(MIN_B, MAX_B);
+        inf.readEoln();
+        
+        if(a == b) {
+            flag = false;
+            assert(flag);
+        }
+    }
+
     inf.readEof();
 }
