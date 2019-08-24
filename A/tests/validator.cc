3da5dc1238f5b48b34c0225285e0c68e69656f8d
fuu32 <owl.gr2@icloud.com>
2019-08-24 18:47:39 +0900

    とりあえず動くようにvalidatorを勝手に直してしまった...

--- A/tests/validator.cc
+++ A/tests/validator.cc
@@ -1,26 +1,28 @@
 #include "./testlib.h"
 #include "./constraints.hpp"
 #include <cassert>
-#include<assert.h>
-
+#include <assert.h>
+using namespace std;
 int main(){
     registerValidation();
-    inf.readInt(MIN_X, MAX_X);
+    int X=inf.readInt(MIN_X, MAX_X);
     inf.readSpace();
-    inf.readInt(MIN_T, MAX_T);
+    int T=inf.readInt(MIN_T, MAX_T);
     inf.readSpace();
-    inf.readInt(MIN_S, MAX_S);
+    int S=inf.readInt(MIN_S, MAX_S);
     inf.readEoln();
     int n = inf.readInt(MIN_N, MAX_N);
     inf.readEoln();
     bool flag = true;
     for(int i = 0; i < n; ++i){
-        std::string s = inf.readString();
-        if(s != "nobiro" && s != "tidime" && s != "karero"){
-            flag = false;
-            assert(flag);
+        string s = inf.readString();
+        if(s == "nobiro" || s == "tidime" || s == "karero"){
+          // ok
+        }else{
+          flag = false;
+          assert(flag);
         }
-        inf.readEoln();
+        //inf.readEoln();
     }
     inf.readEof();
 }
