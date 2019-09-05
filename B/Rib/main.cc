5db14659add6896be86caeca95031b029d13369d
renkarsu <mutou0604@outlook.jp>
2019-09-05 13:00:36 +0900

    add main.cc

--- B/Rib/main.cc
+++ B/Rib/main.cc
@@ -8,47 +8,27 @@ signed main()
 {
     int n; cin >> n;
     string s; cin >> s;
-    map<char, int> mp;
-    for(int i{}; i < (int)s.size(); ++i)
+    /*
+           north(y+=1)
+            ↑
+west(x-=1)←　→east(x+=1)
+            ↓
+           south(y-=1)
+    */
+    int north{}, south{}, east{}, west{};
+    for (auto &i : s)
     {
-        mp[s[i]]++;
+        if     ('A' <= i && i <= 'M') ++north;
+        else if('N' <= i && i <= 'Z') ++south;
+        else if('a' <= i && i <= 'm') ++east;
+        else if('n' <= i && i <= 'z') ++west;
     }
-    int north{};
-    int west{};
-    int east{};
-    int south{};
-    for(auto a: mp)
-    {
-        const char b = a.first;
-        if(b=='A'||b=='B'||b=='C'||b=='D'||b=='E'||b=='F'
-         ||b=='G'||b=='H'||b=='I'||b=='J'||b=='K'||b=='L'||b=='M')
-        {
-             ++north;
-        }
-        else if(b=='N'||b=='O'||b=='P'||b=='Q'||b=='R'||b=='S'
-               ||b=='T'||b=='U'||b=='V'||b=='W'||b=='X'||b=='Y'||b=='Z')
-        {
-            ++south;
-        }
-        else if(b=='a'||b=='b'||b=='c'||b=='d'||b=='e'||b=='f'
-              ||b=='g'||b=='h'||b=='i'||b=='j'||b=='k'||b=='l'||b=='m')
-        {
-            ++east;
-        }
-        else
-        {
-            ++west;
-        }
-    }
-    int sum = north+south+east+west;
-    cout << sum << '\n';
-    for(int i = 0; i < north; ++i)
-        cout << 'A';
-    for(int i = 0; i < south; ++i)
-        cout << 'N';
-    for(int i = 0; i < east; ++i)
-        cout << 'a';
-    for(int i = 0; i < west; ++i)
-        cout << 'n';
+    cout << north-south+east-west << '\n';
     
+    for(int i{}; i < north-south; ++i) cout << 'A';
+    for(int i{}; i < south-north; ++i) cout << 'N';
+    for(int i{}; i < east-west; ++i)   cout << 'a';
+    for(int i{}; i < west-east; ++i)   cout << 'n';
+
+    cout << '\n';
 }
