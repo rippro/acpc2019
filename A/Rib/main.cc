6e511fca18987c06d9e022b086dd01c19a855e0d
renkarsu <mutou0604@outlook.jp>
2019-09-05 11:38:39 +0900

    complete A rib's solution

--- A/Rib/main.cc
+++ A/Rib/main.cc
@@ -1,17 +1,18 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int main(){
-  int X,T,S;
-  cin>>X>>T>>S;
-  int N;
-  cin>>N;
-  for(int i=0;i<N;i++){
-    string s;
-    cin>>s;
-    if(s=="nobiro")X=max(X+T,0);
-    else if(s=="tidime")X=max(X+S,0);
-    else X=0;
-  }
-  cout<<X<<endl;
-  return 0;
+//#define int long long
+
+signed main()
+{
+    int x,t,s; cin >> x >> t >> s;
+    int n; cin >> n;
+    for(int i{}; i < n; ++i)
+    {
+        string ss; cin >> ss;
+        if     (ss == "nobiro") x = max(x+t, 0);
+        else if(ss == "tidime") x = max(x+s, 0);
+        else if(ss == "karero") x = 0;
+    }
+
+    cout << x << '\n';
 }
