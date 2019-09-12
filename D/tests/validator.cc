a8c19bd42e0c3a3f9cd944ae41b41101da23f0d6
TMiya3 <is0283ir@ed.ritsumei.ac.jp>
2019-09-12 18:13:42 +0900

    Dのvalidatorを勝手に修正

--- D/tests/validator.cc
+++ D/tests/validator.cc
@@ -13,7 +13,7 @@ int main(){
 	int E = inf.readInt(1, N);
 	inf.readEoln();
 
-	for (int i = 0; i < N; i++) {
+	for (int i = 1; i < N; i++) {
 		int a = inf.readInt(1, N);
 		inf.readSpace();
 		int b = inf.readInt(1, N);
