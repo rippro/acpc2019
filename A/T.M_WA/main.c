720e849ba84f0049e19d09e615ebc8911fc52557
TMiya3 <is0283ir@ed.ritsumei.ac.jp>
2019-09-16 16:36:07 +0900

    AにT.Mの誤解法を追加

---
+++ A/T.M_WA/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+  int n,a,b,m;
+  char s[10];
+  scanf("%d %d %d %d",&n,&a,&b,&m);
+  while(m--){
+    scanf("%s",s);
+    if(s[0]=='n')n+=a;
+    if(s[0]=='t')n+=b;
+    if(s[0]=='k')n=0;
+  }
+  if(n<0)n=0;
+  printf("%d\n",n);
+  return 0;
+}
+  
